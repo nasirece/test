0eec0fb0b8d87009d74a43fd1de916b44c6250ed
nasirece <nasirece@yahoo.com>
2013-05-26 16:05:58 +0600

    add form

---
+++ form/form/storage/fb_error.log.php
@@ -0,0 +1,3 @@
+<?php echo 'Access denied.'; exit(); ?>
+Mon, 25 Feb 2013 05:21:50 +0000: Couldn't open or read: D:/xampp/htdocs/form/form.html
+Mon, 25 Feb 2013 05:21:53 +0000: Couldn't open or read: D:/xampp/htdocs/form/form.html
---
+++ form/form/validation_data.js
@@ -0,0 +1,3 @@
+ var data_validation = '{"fb-submit-button":{"hover":{"background-image":""}},"item1_text_1":{"required":true},"item2_password_1":{"minlength":5,"required":true},"item3_text_1":{},"item4_text_1":{},"item5_select_1":{}}';
+ var data_jsplugins = '[]';
+ var data_cssplugins = '[]';
